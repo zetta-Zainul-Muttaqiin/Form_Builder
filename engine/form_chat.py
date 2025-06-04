4c913b50e31eb7e002a0e39a6ec43c85da6f503d
zetta-Zainul-Muttaqiin <zainul.muttaqin@zettabyte.life>
2025-06-04 16:04:27 +0700

    [AI_009] Change print with Logger

--- engine/form_chat.py
+++ engine/form_chat.py
@@ -15,6 +15,8 @@ from models.llms                import LLMModels
 
 from engine.rag_engine          import init_astradb_retriever
 
+from setup import LOGGER
+
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
     # in the annotation defines how this state key should be updated
@@ -42,26 +44,26 @@ def question_suggester(state: FormAssistantState) -> FormAssistantState:
     # Example LLM logic
     result = LLMModels().nano.invoke(f"Suggest new questions for this form:\n{state['form']}")
     state["suggested_questions"] = result.json()
-    print("question_suggester: ", state.get("suggested_questions", "None"))
+    LOGGER.info("question_suggester: ", state.get("suggested_questions", "None"))
     return state
 
 def command_suggester(state: FormAssistantState) -> FormAssistantState:
     result = LLMModels().nano.invoke(f"Suggest commands to edit the form:\n{state['form']}")
     state["suggested_commands"] = result.json()
-    print("command_suggester: ", state.get("suggested_commands", "None"))
+    LOGGER.info("command_suggester: ", state.get("suggested_commands", "None"))
     return state
 
 def template_retriever(state: FormAssistantState) -> FormAssistantState:
     # You can add vector-based retrieval here
     retriever = init_astradb_retriever()
-    state["retrieved_templates"] = retriever(state["user_input"])
-    print("template_retriever: ", state.get('retrieved_templates', "None"))
+    state["retrieved_templates"] = retriever.invoke(state["user_input"])
+    LOGGER.info("template_retriever: ", state.get('retrieved_templates', "None"))
     return state
 
 def context_extractor(state: FormAssistantState) -> FormAssistantState:
     # Optional: Parse form into structured metadata
     state["form_structure"] = f"Current Form Generated {state['form']}"
-    print("context_extractor: ", state.get("form_structure", "None"))
+    LOGGER.info("context_extractor: ", state.get("form_structure", "None"))
     return state
 
 def llm_response_generator(state: FormAssistantState) -> FormAssistantState:
@@ -77,7 +79,7 @@ def llm_response_generator(state: FormAssistantState) -> FormAssistantState:
     Reply with a user-facing message.
     """)
     state["messages"].append({"role": "assistant", "content": result.content})
-    print("llm_response_generator: ", state.get("messages", "None"))
+    LOGGER.info("llm_response_generator: ", state.get("messages", "None"))
     return state
 
 
@@ -115,16 +117,16 @@ def input_analyzer(state: FormAssistantState) -> FormAssistantState:
         parsed = output_parser.parse(raw_response.content)
         state["analysis"] = parsed.model_dump()
     except Exception as e:
-        print("Parser Error:", e)
+        LOGGER.info("Parser Error:", e)
         state["analysis"] = {"intent": "unclear", "reason": "Could not parse response properly."}
 
-    print("input_analyzer:", state["analysis"])
+    LOGGER.info("input_analyzer:", state["analysis"])
     return state
 
 
 def suggestion_condition(state: FormAssistantState) -> str:
     intent = state["analysis"]["intent"]
-    print("suggestion_condition: ", intent)
+    LOGGER.info("suggestion_condition: ", intent)
     if intent == "suggest_questions":
         return "question_suggester"
     elif intent == "suggest_command":
@@ -137,7 +139,7 @@ def suggestion_condition(state: FormAssistantState) -> str:
 def chatbot(state: FormAssistantState) -> FormAssistantState:
     if not state.get("original_prompt"):
         state["original_prompt"] = state["user_input"]
-    print("chatbot: ", state.get("messages", "None"))
+    LOGGER.info("chatbot: ", state.get("messages", "None"))
     return state
 
 
@@ -187,7 +189,7 @@ def run_form_assist(input: str, current_form: dict,  messages:list=[]):
         "form": current_form,
         "messages": messages
     })
-    print("RESULT: ", result)
+    
     answer = result['messages'][-1]['content']
     append_meesages = [
         {
